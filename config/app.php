c6d0d52efa5de7ea20823a7024f2cefbb612c1f4
Iterl <iterl054@gmail.com>
2019-03-18 03:20:49 +0200

    make environmental dependent configs

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'debug' => true,
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'url' => 'http://october-cms.loc/',
+    'url' => env('APP_URL', 'http://october-cms.loc/'),
 
     /*
     |--------------------------------------------------------------------------
@@ -101,7 +101,7 @@ return [
     |
     */
 
-    'key' => 'ZOQCMez24LqZrphtluajXPzgdOleNSks',
+    'key' => env('APP_KEY', ''),
 
     'cipher' => 'AES-256-CBC',
 
