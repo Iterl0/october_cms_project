09238fb415ab889b3498a2aaf044bfeebff020df
Iterl <iterl054@gmail.com>
2019-04-19 18:32:46 +0300

    add fields to user model

--- plugins/khelo/playerprofile/updates/add_fields_to_user.php
+++ plugins/khelo/playerprofile/updates/add_fields_to_user.php
@@ -10,6 +10,13 @@ class AddFieldsToUser extends Migration
     {
         Schema::table('users', function(Blueprint $table) {
             $table->string('mobile_number')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->char('gender', 1)->nullable();
+            $table->string('address1')->nullable();
+            $table->string('address2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('pin')->nullable();
         });
     }
 
@@ -17,6 +24,13 @@ class AddFieldsToUser extends Migration
     {
         Schema::table('users', function(Blueprint $table) {
             $table->dropColumn('mobile_number');
+            $table->dropColumn('birth_date');
+            $table->dropColumn('gender');
+            $table->dropColumn('address1');
+            $table->dropColumn('address2');
+            $table->dropColumn('city');
+            $table->dropColumn('state');
+            $table->dropColumn('pin');
         });
     }
 }
